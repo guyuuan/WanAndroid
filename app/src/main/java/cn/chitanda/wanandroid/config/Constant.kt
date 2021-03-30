ddb111e39959390c9f6ad11bfae2df7d5691abe1
chen <chunjinchen1998@gmail.com>
2021-03-30 17:47:24 +0800

    1.MMKV存储键使用常量定义 2.启动页数据不再每次启动都去更新,只在每天第一次打开时更新
    1.MMKV存储键使用常量定义
    2.启动页数据不再每次启动都去更新,只在每天第一次打开时更新

---
+++ app/src/main/java/cn/chitanda/wanandroid/config/Constant.kt
@@ -0,0 +1,12 @@
+package cn.chitanda.wanandroid.config
+
+/**
+ * @Author:       Chen
+ * @Date:         2021/3/30 17:40
+ * @Description:
+ */
+object Constant {
+    const val KEY_SPLASH_CACHED_TIME = "splash_cached_time"
+    const val KEY_HOME_ARTICLES_SCROLL_STATUS_POSITION = "home_articles_scroll_status_position"
+    const val KEY_HOME_ARTICLES_SCROLL_STATUS_OFFSETS = "home_articles_scroll_status_offsets"
+}
